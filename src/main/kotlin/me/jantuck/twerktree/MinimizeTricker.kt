a2161469fb3244c40a3b667b7db563c8c57dc396
Jan Tuck <jan@tuck.dk>
2020-07-30 01:17:11 +0200

    Do stuff

--- src/main/kotlin/me/jantuck/twerktree/MinimizeTricker.kt
+++ src/main/kotlin/me/jantuck/twerktree/MinimizeTricker.kt
@@ -1,5 +1,5 @@
 package me.jantuck.twerktree
 
 object MinimizeTricker {
-    val providerv1_16 =  me.jantuck.twerktree.compatibility.v1_16.Provider()
+    val providerv1_16 by lazy { me.jantuck.twerktree.compatibility.v1_16.Provider() }
 }
